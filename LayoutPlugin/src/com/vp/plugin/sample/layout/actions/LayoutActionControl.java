d3f8e732857a4990585102f5dc88d27bc5b1c0c3
Bjorn <s41nn0n@gmail.com>
2018-08-21 09:13:01 +0200

    Format all diagrams
    Testing

--- LayoutPlugin/src/com/vp/plugin/sample/layout/actions/LayoutActionControl.java
+++ LayoutPlugin/src/com/vp/plugin/sample/layout/actions/LayoutActionControl.java
@@ -15,41 +15,46 @@ public class LayoutActionControl implements VPActionController {
 	@Override
 	public void performAction(VPAction arg0) {
 		// Obtain DiagramManager from ApplicationManager 
-		DiagramManager diagramManager = ApplicationManager.instance().getDiagramManager(); 
-		IDiagramUIModel diagram = diagramManager.getActiveDiagram();
+// 		DiagramManager diagramManager = ApplicationManager.instance().getDiagramManager(); 
+// 		IDiagramUIModel diagram = diagramManager.getActiveDiagram();
 		
-		// Initialize different LayoutOptions
-		LayoutOption$Orthogonal orthogonalLayoutOption = diagramManager.createOrthogonalLayoutOption();
-		orthogonalLayoutOption.setLayoutGridSize(100);
-		
-		LayoutOption$Hierarchical hierarchicalLayoutOption = diagramManager.createHierarchicalLayoutOption();
-		hierarchicalLayoutOption.setMinimumConnectorDistance(10);
-		hierarchicalLayoutOption.setMinimumLayerDistance(20);
-		hierarchicalLayoutOption.setMinimumShapeDistance(30);
-		
-		LayoutOption$DirectedTree directedTreeLayoutOption = diagramManager.createDirectedTreeLayoutOption();
-		directedTreeLayoutOption.setMinimumLaterDistance(50);
-		directedTreeLayoutOption.setMinimumShapeDistance(50);
-		directedTreeLayoutOption.setOrientation(LayoutOption$Orientation.TopToBottom);
-		
-		// Generate random number to perform different layout 
-		int min = 0;
-		int max = 2;
-		int range = max - min + 1;
-		
-		int rand = (int) (Math.random() * range) + min;
-		System.out.println("rand: " + rand);
-		switch (rand) {
-			//passing different option to perform layout
-			case 0: diagramManager.openAndLayoutDiagram(diagram, orthogonalLayoutOption);
-					break;
-			
-			case 1: diagramManager.openAndLayoutDiagram(diagram, hierarchicalLayoutOption);
-					break;
+		IProject project = ApplicationManager.instance().getProjectManager().getProject();
+		IDiagramUIModel[] diagrams = project.toDiagramArray();
+		for (int diagram : diagrams){
+			// Initialize different LayoutOptions
+// 			LayoutOption$Orthogonal orthogonalLayoutOption = diagramManager.createOrthogonalLayoutOption();
+// 			orthogonalLayoutOption.setLayoutGridSize(100);
 
-			case 2: diagramManager.openAndLayoutDiagram(diagram, directedTreeLayoutOption);
-					break;
-		}		
+			LayoutOption$Hierarchical hierarchicalLayoutOption = diagramManager.createHierarchicalLayoutOption();
+			hierarchicalLayoutOption.setMinimumConnectorDistance(10);
+			hierarchicalLayoutOption.setMinimumLayerDistance(20);
+			hierarchicalLayoutOption.setMinimumShapeDistance(30);
+
+// 			LayoutOption$DirectedTree directedTreeLayoutOption = diagramManager.createDirectedTreeLayoutOption();
+// 			directedTreeLayoutOption.setMinimumLaterDistance(50);
+// 			directedTreeLayoutOption.setMinimumShapeDistance(50);
+// 			directedTreeLayoutOption.setOrientation(LayoutOption$Orientation.TopToBottom);
+
+			diagramManager.openAndLayoutDiagram(diagram, hierarchicalLayoutOption);
+// 			// Generate random number to perform different layout 
+// 			int min = 0;
+// 			int max = 2;
+// 			int range = max - min + 1;
+
+// 			int rand = (int) (Math.random() * range) + min;
+// 			System.out.println("rand: " + rand);
+// 			switch (rand) {
+// 				//passing different option to perform layout
+// 				case 0: diagramManager.openAndLayoutDiagram(diagram, orthogonalLayoutOption);
+// 						break;
+
+// 				case 1: diagramManager.openAndLayoutDiagram(diagram, hierarchicalLayoutOption);
+// 						break;
+
+// 				case 2: diagramManager.openAndLayoutDiagram(diagram, directedTreeLayoutOption);
+// 						break;
+// 			}	
+		}
 	}		
 
 	@Override
